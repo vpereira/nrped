37cc939fc4caced9d634d3e11ad17868bce7ebed
Victor Pereira <vpereira@suse.de>
2014-05-05 22:46:52 +0200

    improved tests. I love go :P

--- read_config/read_config_test.go
+++ read_config/read_config_test.go
@@ -11,10 +11,24 @@ func TestReadConfigInit(t *testing.T) {
         t.Error("Init failed to set FileName")
     }
 }
+
 func TestReadConfigReadFileConfig(t *testing.T) {
     obj := new(ReadConfig)
     obj.Init("nrpe-test.cfg")
     if err := obj.ReadConfigFile(); err != nil {
         t.Error("ReadConfigFile failed to read config file")
+    }
+}
+
+func TestReadConfigReadCommands(t *testing.T) {
+    obj := new(ReadConfig)
+    obj.Init("nrpe-test.cfg")
+    obj.ReadConfigFile()
+    obj.ReadCommands()
+    if len(obj.AllowedCommands) == 0 {
+        t.Error("ReadCommands failed to parse nrpe commands")
+    }
+    if val,ok := obj.AllowedCommands["check_iostat"]; ok == false {
+        t.Error("ReadCommands failed to parse nrpe commands")
     }
 }
