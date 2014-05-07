f0ca02345ff4d18cd9d66accd0f9885595f8a38d
Victor Pereira <vpereira@suse.de>
2014-05-07 22:12:45 +0200

    updated build.sh, because it was failing at travis-ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -31,8 +31,8 @@ cd common && go build common.go && cd ..
 cd read_config && go build read_config.go && cd ..
 cd check_nrpe && go build -o check_nrpe && cd ..
 go build  -o nrped
-cp nrped $GOPATH/bin
-cp check_nrpe/check_nrpe $GOPATH/bin
+#cp nrped $GOPATH/bin
+#cp check_nrpe/check_nrpe $GOPATH/bin
 #go build \
 #    -ldflags "-X main.GitCommit ${GIT_COMMIT}${GIT_DIRTY}" \
 #   -v \
