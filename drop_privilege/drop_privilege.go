4cea08c0e36827c31184c04d69e60c3775df6802
Victor Pereira <vpereira@suse.de>
2014-05-15 21:34:53 +0200

    drop_privilege added

---
+++ drop_privilege/drop_privilege.go
@@ -0,0 +1,43 @@
+package drop_privilege
+/*
+#include <sys/types.h>
+#include <pwd.h>
+#include <stdlib.h>
+*/
+import "C" //strange, it cannot be declared together with the others import.. is it a golang bug?
+
+import (
+    "unsafe"
+    "syscall"
+)
+
+
+
+type Passwd struct {
+    Uid   uint32
+    Gid   uint32
+    Dir   string
+    Shell string
+}
+
+func Getpwnam(name string) *Passwd {
+    cname := C.CString(name)
+    defer C.cfree(unsafe.Pointer(cname))
+    cpw := C.getpwnam(cname)
+    return &Passwd{
+        Uid: uint32(cpw.pw_uid),
+        Gid: uint32(cpw.pw_gid),
+        Dir: C.GoString(cpw.pw_dir),
+        Shell: C.GoString(cpw.pw_shell),
+    }
+}
+
+func DropPrivileges(uid int, gid int) (error, bool) {
+    if err := syscall.Setuid(uid); err != nil {
+        return err,false
+    }
+    if err := syscall.Setgid(gid); err != nil {
+        return err,false
+    }
+    return nil, true
+}
---
+++ drop_privilege/drop_privilege_test.go
@@ -0,0 +1,12 @@
+package drop_privilege
+
+import (
+    "testing"
+)
+
+func TestGetpwnam(t *testing.T) {
+    pwd := Getpwnam("nobody")
+    if pwd == nil {
+        t.Error("getpwnam failed")
+    }
+}
