c788608d74bb691fb381df19372f4652c51d4357
Victor Pereira <vpereira@suse.de>
2015-02-10 15:49:59 +0100

    check_nrpe: added support for TLS

--- check_nrpe/main.go
+++ check_nrpe/main.go
@@ -6,15 +6,28 @@ import (
 	"github.com/vpereira/nrped/common"
 	"net"
 	"os"
+	"crypto/tls"
 )
 
-func prepareConnection(endpoint string) net.Conn {
+func getSocket(transport_type int, endpoint string,tcpAddr *net.TCPAddr) (net.Conn, error) {
+	switch transport_type {
+		case 0:
+			return net.DialTCP("tcp", nil, tcpAddr)
+		case 1:
+			return tls.Dial("tcp",endpoint,nil)
+		case 2:
+			return nil,nil //implement it
+	}
+	return nil,nil
+}
+
+func prepareConnection(endpoint string,transport_type int) net.Conn {
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", endpoint)
 	common.CheckError(err)
+	conn,err := getSocket(transport_type,endpoint,tcpAddr)
 
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	common.CheckError(err)
-	
+
 	if conn != nil {
 		return conn
 	}
@@ -30,12 +43,13 @@ func main() {
 
 	var host = goopt.String([]string{"-H", "--host"}, "127.0.0.1", "The remote host running NRPE-Server")
 	var port = goopt.Int([]string{"-p", "--port"}, 5666, "The remote port on which the NRPE-server listens")
+	var transport = goopt.Int([]string{"-t","--transport"},0,"Transport type: 0 - clear, 1 - ssl, 2 -ssh")
 	var command = goopt.String([]string{"-c", "--command"}, "version",
 		"The check command defined in the nrpe.cfg file you would like to trigger")
 
 	goopt.Parse(nil)
 	service := fmt.Sprintf("%s:%d", *host, *port)
-	conn := prepareConnection(service)
+	conn := prepareConnection(service,*transport)
 	pkt_to_send := common.PrepareToSend(*command, common.QUERY_PACKET)
 	err := common.SendPacket(conn, pkt_to_send)
 	common.CheckError(err)
